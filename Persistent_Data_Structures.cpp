0bd2784954f06db0bf1647529f982cc4c8eda3c0
Anaksimander <61375814+Anaksimander@users.noreply.github.com>
2020-12-17 15:32:31 +0700

    Update Persistent_Data_Structures.cpp

--- Persistent_Data_Structures.cpp
+++ Persistent_Data_Structures.cpp
@@ -3,86 +3,112 @@
 #include <vector>
 using namespace std;
 
-//структура хранение версий. т.е. тип данных узла
-template <typename T>
-struct FatNode 
-{
-public:
-	T value;
-	int version = 0;
-	int parentVersion = -1;
-	int numLastChangeElem = -1;
-};
+
+
 
 //класс персистентный массив
-template <typename T>
-class PersistMass   
+template <class T>
+class PersistMass
 {
 public:
-	PersistMass(int n);								//конструктор с одним параметром
+	//структура хранение версий. т.е. тип данных узла
+	class FatNodes_elem
+	{
+	public:
+		T value = 0 ;
+		int version = 0;
+	};
+
+	PersistMass();
+	PersistMass(int n);							//конструктор с одним параметром
 	void set(int i, T value);					//ввод элемента
 	T get(int i);								//вывод элемента последней версии 
 	T get(int i, int version);					//вывод элемента выбранной версии 
 	int getSize();								//получить размер массива
-	void undo();
+	void add(T value);							//добавить элемент в массив
+	void empty();								//опустошить массив
+	T& operator[](const int i);					//перегрузка оператора[]	
+	
+protected:
+	int version;								//последняя версия структуры, счетчик
+	int useVersion;								//используемая версия (для undo/redo)
+	bool inItSelf;								//вложенный сам в себя
 
-	T operator[](int i) const;					//перегрузка оператора[]
-	T& operator=(const T& elem);				//перегрузка оператора = элемента
-
-private:
-	int version;							//последняя версия структуры, счетчик
-	int useVersion;							//используемая версия (для undo/redo)
-	int numLastChangeElem;					//номер последнего измененного элемента
-	int SizePersistMass;						//размер массива
-	vector <vector <FatNode<T>>> vectorPersistMass;//хранение данных. внутренний вектр это толстый узел
+	int maxSize;								//максимальный размер массива
+	int SizePersistMass;						//размер массива этой версии 
+	vector <vector <FatNodes_elem>> vectorFatNode;//хранение данных. внутренний вектр это толстый узел
 };
 
+//конструктор без параметров
+template <class T>
+PersistMass<T>::PersistMass()
+{
+	version = 0;
+	useVersion = 0;
+
+	SizePersistMass = 0;
+	vectorFatNode.resize(0);
+	//vectorFatNode;
+}
+
 //конструктор с одним параметром
-template <typename T>
+//template <class T>
+//PersistMass<PersistMass<T>>::PersistMass(int n)
+//{
+//
+//}
+//конструктор с одним параметром
+template <class T>
 PersistMass<T>::PersistMass(int n)
 {
 	version = 0;
 	useVersion = 0;
-	numLastChangeElem = -1;				//не изменяли
 	SizePersistMass = n;
-	vectorPersistMass.resize(n, vector <FatNode<T>>(1));
+	vectorFatNode.resize(n);
 
-	for (int i = 0; i < n; i++) {
-		vectorPersistMass[i][0].version = 0;
-		vectorPersistMass[i][0].value = {};
-		vectorPersistMass[i][0].parentVersion = -1;
+	if (typeid(T) == typeid(PersistMass<int>)) {
+		inItSelf = true;
+		FatNodes_elem box;
+		for (int i = 0; i < n; i++) {
+			vectorFatNode[i].push_back(box);
+		}
+	}
+	else {
+		inItSelf = false;
+		FatNodes_elem box;
+		for (int i = 0; i < n; i++) {
+			box.version = 0;
+			box.value = {};
+			vectorFatNode[i].push_back(box);
+		}
 	}
 }
 
 //ввод элемента
-template <typename T>
+template <class T>
 void PersistMass<T>::set(int i, T value)
 {
 	if (i < 0 || i >= SizePersistMass)
 		exit(1);
-
-	FatNode<T> box;
 	version++;
-	box.value = value;	
+
+	FatNodes_elem box;
+	//расширяем узел новым значением
+	box.value = value;
 	box.version = version;
-	box.parentVersion = useVersion;
-	box.numLastChangeElem = numLastChangeElem;
 
 	useVersion = version;
-	numLastChangeElem = i;
-	vectorPersistMass[i].push_back(box);
+	vectorFatNode[i].push_back(box);
 }
 
 //вывод элемента последней версии 
-template <typename T>
+template <class T>
 T PersistMass<T>::get(int i)
 {
 	if (i < 0 || i >= SizePersistMass)
 		exit(1);
 	if (useVersion == version) {
-		FatNode<T> box;
-		box = vectorPersistMass[i].back();//последняя версия
-		return vectorPersistMass[i].back().value;
+		return vectorFatNode[i].back().value;
 	}
 	else {
 		return get(i, useVersion);
@@ -90,7 +116,7 @@ T PersistMass<T>::get(int i)
 }
 
 //вывод элемента выбранной версии 
-template <typename T>
+template <class T>
 T PersistMass<T>::get(int i, int inVersion)
 {
 	if (i < 0 || i >= SizePersistMass)
@@ -104,84 +130,91 @@ T PersistMass<T>::get(int i, int inVersion)
 		exit(1);
 	}
 
-	int size = vectorPersistMass[i].size() - 1;
+	int size = vectorFatNode[i].size() - 1;
 	//как только inVersion больше чем 
-	while (inVersion < vectorPersistMass[i][size].version) {
+	while (inVersion < vectorFatNode[i][size].version) {
 		size--;
 	}
-	return vectorPersistMass[i][size].value;
+	return vectorFatNode[i][size].value;
 }
 
 //получить размер массива
-template <typename T>
+template <class T>
 int PersistMass<T>::getSize() {
 	return SizePersistMass;
 }
 
 //перегрузка оператора[]
-template <typename T>
-T PersistMass<T>::operator[](int i) const
+template <class T>
+T& PersistMass<T>::operator[](const int i)
 {
 	if (i < 0 || i >= SizePersistMass)
 		exit(1);
+
+	return vectorFatNode[i].back().value;
+}
+
+template <class T>
+void PersistMass<T>::add(T value) 
+{
 	
-	return vectorPersistMass[i].back().value;
+	SizePersistMass++;
+	version++;
+	useVersion = version;
+	FatNodes_elem box;
+	vector <FatNodes_elem> vect_box;
+	box.version = version;
+	box.value = value;
+	vect_box.push_back(box);
+	vectorFatNode.insert(vectorFatNode.begin(),vect_box);
 }
 
-//получить размер массива
-template <typename T>
-void PersistMass<T>::undo() {
-	if (useVersion == 0) {
-		cout << "use version 0. You can't now use 'undo'" << endl;
-		exit(1);
-	}
-
-	int size = vectorPersistMass[numLastChangeElem].size() - 1;
-	//как только inVersion больше чем 
-	while (useVersion < vectorPersistMass[numLastChangeElem][size].version) {
-		size--;
-	}
-
-	FatNode<T> box;
-	box = vectorPersistMass[numLastChangeElem][size];//последняя версияsize--;
-	useVersion = box.parentVersion;						//смена используемой версии
-	numLastChangeElem =box.numLastChangeElem;	//запись последнего измененного элемента в этой версии 
+template<class T>
+void PersistMass<T>::empty()
+{
+	this->SizePersistMass = 0;
 }
+
+////перегрузка оператора = элемента
+//template <class T>
+//FatNodes_elem<T>& PersistMass<T>::operator=FatNodes_elem<T>(const T& elem) {
+//
+//	return elem.version;
+//}
 
 int main()
 {
+
 	PersistMass<int> mass1(2);//выделение памяти
-	/*cout << "output elemrnt 0: \t" << mass1.outPutElement(0) << endl;
-	cout << "mass[0] = \t\t" << mass1[0] << endl;*/
-	cout << "output elemrnt 0 : " << mass1.get(0) << endl;
+	mass1.add(2);
+	mass1.set(2, 3);
+	mass1.empty();
+	mass1.add(3);
+	//PersistMass<int> mass(mass1);//выделение памяти
+
+	//PersistMass <PersistMass<int>> mass2(2);//выделение памяти
+	//mass2.set(0, mass1);
+
+	/*cout << "used 'set' :" << endl;
+	cout << "output elemrnt 0 : " << mass1[0] << endl;
 	cout << "output elemrnt 1 : " << mass1.get(1) << endl;
 	cout << "===========================" << endl;
+	 
 	mass1.set(0, 1);
-	cout << "output elemrnt 0 : " << mass1.get(0) << endl;
+	cout << "output elemrnt 0 : " << mass1[0] << endl;
 	cout << "output elemrnt 1 : " << mass1.get(1) << endl;
-	cout << "===========================" << endl;
-	mass1.set(1, 2);
-	cout << "output elemrnt 0 : " << mass1.get(0) << endl;
-	cout << "output elemrnt 1 : " << mass1.get(1) << endl;
-	cout << "===========================" << endl;
-	mass1.set(0, 3);
-	cout << "output elemrnt 0 : " << mass1.get(0) << endl;
-	cout << "output elemrnt 1 : " << mass1.get(1) << endl;
-	cout << "===========================" << endl;
-	cout << "===========================" << endl;
-	cout << "===========================" << endl;
-	mass1.undo();
-	cout << "output elemrnt 0 : " << mass1.get(0) << endl;
-	cout << "output elemrnt 1 : " << mass1.get(1) << endl;
-	cout << "===========================" << endl;
-	mass1.undo();
-	cout << "output elemrnt 0 : " << mass1.get(0) << endl;
-	cout << "output elemrnt 1 : " << mass1.get(1) << endl;
-	cout << "===========================" << endl;
-	mass1.undo();
-	cout << "output elemrnt 0 : " << mass1.get(0) << endl;
-	cout << "output elemrnt 1 : " << mass1.get(1) << endl;
-	cout << "===========================" << endl;
+	cout << "===========================" << endl;*/
+	//mass1.set(1, 2);
+	//cout << "output elemrnt 0 : " << mass1[0] << endl;
+	//cout << "output elemrnt 1 : " << mass1.get(1) << endl;
+	//cout << "===========================" << endl;
+	//mass1.set(0, 3);
+	////mass1[0] = 0;
+	//cout << "output elemrnt 0 : " << mass1[0] << endl;
+	//cout << "output elemrnt 1 : " << mass1.get(1) << endl;
+	//cout << "===========================\n" << endl;
+
+
 	return 0;
 }
 
